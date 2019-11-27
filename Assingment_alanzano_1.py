3ce330c737ccdf732f7041a9eb339f8776a8ab08
Alex Lanzano <alanzano579@gmail.com>
2019-11-27 15:54:24 -0700

    Update Assingment_alanzano_1.py
    Viewing data through filtering function.

--- Assingment_alanzano_1.py
+++ Assingment_alanzano_1.py
@@ -8,14 +8,29 @@ This is a temporary script file.
 import urllib2
 import xml.etree.ElementTree as ET
 
+def FilterData(x):
+    try:
+        return x.text
+    except:
+        return ''
 
 data = urllib2.urlopen('http://syndication.enterprise.websiteidx.com/feeds/BoojCodeTest.xml')
 
 tree = ET.parse(data)
 root = tree.getroot()
 
-print root.findall('Listing')
-
+for i in root:
+    StreetAddress = FilterData(i.find('Location').find('StreetAddress'))
+    Price = FilterData(i.find('ListingDetails').find('Price'))
+    MlsId = FilterData(i.find('ListingDetails').find('MlsId'))
+    MlsName = FilterData(i.find('ListingDetails').find('MlsName'))
+    DateListed = FilterData(i.find('ListingDetails').find('DateListed'))
+    Description = FilterData(i.find('BasicDetails').find('Description'))
+    Bedrooms = FilterData(i.find('BasicDetails').find('Bedrooms'))
+    FullBathrooms= FilterData(i.find('BasicDetails').find('FullBathrooms'))
+    HalfBathrooms = FilterData(i.find('BasicDetails').find('HalfBathrooms'))
+    ThreeQuarterBathrooms = FilterData(i.find('BasicDetails').find('ThreeQuarterBathrooms'))
+    print StreetAddress
 
 
 
