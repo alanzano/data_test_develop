471975a85abcfba8e4bfcecf96055654a61975ab
Alex Lanzano <alanzano579@gmail.com>
2019-11-30 20:21:24 -0700

    Update Assingment_alanzano_1.py
    Added more concrete comments

--- Assingment_alanzano_1.py
+++ Assingment_alanzano_1.py
@@ -1,3 +1,13 @@
+#####################
+#Code Summary:
+#This code is designed to extract XML data directly from the source and identify tags that are relevant to 
+#the user. The code does this by sending each 'Listing' tag through a function which identifies tags 
+#the user is seeking and returns the relevant text. This data is stored in a single list per 'Listing' tag
+#which refreshes for each 'Listing'. This list is then appended to a maseter 'Final' list if it meets the 
+#requirements of the assignment. Additional cleaning and filtering is done to make the data more acceptable 
+#to the user and is then exported to the CSV file titled 'Output.csv'
+#####################
+
 import urllib2
 import xml.etree.ElementTree as ET
 import csv
@@ -56,6 +66,12 @@ def FilterData_Multi(Listing, item):
 #Fetch data from source                            
 data = urllib2.urlopen('http://syndication.enterprise.websiteidx.com/feeds/BoojCodeTest.xml')
 
+#Should the user wish to alter the code for another XML file or for differenet parameters, make changes to the next section:
+
+#####################
+#For universal applicability chage the following variables:START
+#####################
+
 #Define list which contains tags that require a single response
 Single_item_list = ['MlsId', \
                     'MlsName', \
@@ -76,6 +92,19 @@ Multi_item_list = ['Appliances', \
 list_order = [0,1,2,3,4,5,6,7,8,10,11,9]
 Desc_index = 11 #identify list index where 'description' is located
 Date_index = 2 #identify list index where 'date' is located
+
+Description_length_limit = 200 #200 character limit for description 
+Description_key_word = ' and ' #include 'and' in description 
+Date_lower_limit = '2015-12-31' #date range must be after this
+Date_upper_limit = '2017-01-01' #date rande must be before this 
+
+#Define final header for file 
+Final_header = ['MlsId', 'MlsName', 'DateListed', 'StreetAddress', 'Price', 'Bedrooms', 'FullBathrooms',\
+                     'HalfBathrooms', 'ThreeQuarterBathrooms', 'Appliances', 'Rooms', 'Description']
+
+#####################
+#For universal applicability chage the following variables:STOP
+#####################
 
 #parsing data from source
 tree = ET.parse(data)
@@ -105,16 +134,15 @@ for Listing in root:
     To_CSV = [To_CSV[i] for i in list_order] #orders list with desired header order
     
     #Conditional statement filtering data relevant to description length and date range specifications 
-    if ' and ' in To_CSV[Desc_index][0:200] and To_CSV[Date_index] > '2015-12-31' and To_CSV[Date_index] < '2017-01-01':
-        To_CSV[Desc_index] = To_CSV[Desc_index][0:200]
+    if Description_key_word in To_CSV[Desc_index][0:Description_length_limit] and To_CSV[Date_index] > Date_lower_limit and To_CSV[Date_index] < Date_upper_limit:
+        To_CSV[Desc_index] = To_CSV[Desc_index][0:Description_length_limit]
         To_CSV[Date_index] = To_CSV[Date_index][0:10]
         Final_CSV.append(To_CSV) #appends each line to final list to be exported to CSV
     else:
         pass
 
 Final_CSV = sorted(Final_CSV, key=itemgetter(Date_index)) #orders data in final list by date
-Final_CSV.insert(0, ['MlsId', 'MlsName', 'DateListed', 'StreetAddress', 'Price', 'Bedrooms', 'FullBathrooms',\
-                     'HalfBathrooms', 'ThreeQuarterBathrooms', 'Appliances', 'Rooms', 'Description']) #adds headers
+Final_CSV.insert(0, Final_header) #adds headers
 
 #Exports finalized list to CSV
 with open ('Output.csv', 'w') as csv_file:
