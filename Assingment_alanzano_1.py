6a6d925b07b55150c407d55474a1b6ecfd06c7ea
Alex Lanzano <alanzano579@gmail.com>
2019-11-30 20:04:12 -0700

    Update Assingment_alanzano_1.py
    Finalized code and added comments

--- Assingment_alanzano_1.py
+++ Assingment_alanzano_1.py
@@ -1,6 +1,11 @@
 import urllib2
 import xml.etree.ElementTree as ET
+import csv
+from operator import itemgetter
 
+#Functions 
+
+#Function used to filter tags for elements which require a single response 
 def FilterData_Single(Listing, item):
     if Listing.tag == item:
         return Listing.text
@@ -18,7 +23,8 @@ def FilterData_Single(Listing, item):
                                 return k.text
                             else:
                                 pass
-                            
+
+#Function used to return multiple elements for identified tag                    
 def itering(Itemized, Listing):
     try:
         for a in Listing.find(item).iter('*'):
@@ -27,7 +33,7 @@ def itering(Itemized, Listing):
     except:
         pass
 
-
+#Function used to filter tags for elements which require multiple responses    
 def FilterData_Multi(Listing, item):
     Itemized = []
     if Listing.tag == item:
@@ -46,9 +52,11 @@ def FilterData_Multi(Listing, item):
                                 return itering(Itemized, i)
                             else:
                                 pass
-                            
+
+#Fetch data from source                            
 data = urllib2.urlopen('http://syndication.enterprise.websiteidx.com/feeds/BoojCodeTest.xml')
 
+#Define list which contains tags that require a single response
 Single_item_list = ['MlsId', \
                     'MlsName', \
                     'DateListed', \
@@ -60,41 +68,61 @@ Single_item_list = ['MlsId', \
                     'ThreeQuarterBathrooms', \
                     'Description']
 
+#Define list which contains tags that require multiple responses
 Multi_item_list = ['Appliances', \
                    'Rooms']
 
+#Define header order for output to csv file
 list_order = [0,1,2,3,4,5,6,7,8,10,11,9]
+Desc_index = 11 #identify list index where 'description' is located
+Date_index = 2 #identify list index where 'date' is located
+
 #parsing data from source
 tree = ET.parse(data)
 root = tree.getroot()
 
+#Define list for output to csv file 
 Final_CSV = []
 
+#Main loop iterating through each listing 
 for Listing in root:   
-    To_CSV = []
-    for item in Single_item_list:
-        result = FilterData_Single(Listing, item)
-        if result == [] or result == None:
+    To_CSV = [] #List which refreshes for each listing. Relevant data is appended here, which becomes a line item in final list 
+    for item in Single_item_list: #loop through all required elements that have a single response 
+        result = FilterData_Single(Listing, item) #pass listing tag and relevant header item to function to fetch data element 
+        if result == [] or result == None: #Conditional statement filtering unacceptable entries and replacing them with blanks 
             result = ''
         else:
             pass
         To_CSV.append(result)
-    for item in Multi_item_list:
-        result = FilterData_Multi(Listing, item)
-        if result == [] or result == None:
+    for item in Multi_item_list: #loop through all required elements that have multiple responses
+        result = FilterData_Multi(Listing, item) #pass listing tag and relevant header item to function to fetch data elements
+        if result == [] or result == None: #Conditional statement filtering unacceptable entries and replacing them with blanks 
             result = ''
         else:
             result = ','.join(result)
         To_CSV.append(result)
     
-    Desc_index = 11
-    Date_index = 2
+    To_CSV = [To_CSV[i] for i in list_order] #orders list with desired header order
     
-    To_CSV = [To_CSV[i] for i in list_order]
-    
+    #Conditional statement filtering data relevant to description length and date range specifications 
     if ' and ' in To_CSV[Desc_index][0:200] and To_CSV[Date_index] > '2015-12-31' and To_CSV[Date_index] < '2017-01-01':
         To_CSV[Desc_index] = To_CSV[Desc_index][0:200]
         To_CSV[Date_index] = To_CSV[Date_index][0:10]
-        Final_CSV.append(To_CSV)
+        Final_CSV.append(To_CSV) #appends each line to final list to be exported to CSV
     else:
         pass
+
+Final_CSV = sorted(Final_CSV, key=itemgetter(Date_index)) #orders data in final list by date
+Final_CSV.insert(0, ['MlsId', 'MlsName', 'DateListed', 'StreetAddress', 'Price', 'Bedrooms', 'FullBathrooms',\
+                     'HalfBathrooms', 'ThreeQuarterBathrooms', 'Appliances', 'Rooms', 'Description']) #adds headers
+
+#Exports finalized list to CSV
+with open ('Output.csv', 'w') as csv_file:
+    csv_writer = csv.writer(csv_file, lineterminator = '\n')
+    csv_writer.writerows(Final_CSV)
+
+    
+            
+            
+            
+            
